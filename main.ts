5675a7717f8182838c8b0413418fa4e44a171951
BPM Circuits <65460644+bpmcircuits@users.noreply.github.com>
2024-07-08 12:16:41 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,1 @@
-lineFollower.create(AnalogPin.P0, AnalogPin.P1);
-lineFollower.calibrate();
 
-basic.forever(function() {
-
-    serial.writeLine("Sensor left: " + lineFollower.readLeftSensor());
-    serial.writeLine("Sensor right: " + lineFollower.readRightSensor());
-
-    pause(100);
-});
